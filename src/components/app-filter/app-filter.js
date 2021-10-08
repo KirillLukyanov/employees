63cfed22875dc8a7aca90dd90b9da9638d5b7ba6
unknown <kirlukyanov@gmail.com>
2021-10-08 19:50:59 +0300

    Done

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,22 +1,78 @@
+import { Component } from "react";
+
 import "./app-filter.css";
 
-const AppFilter = () => {
-    return (
-        <div className="btn-group">
-            <button type="button"
-                    className="btn btn-light">
-                    Все сотрудники
-            </button>
-            <button type="button"
-                    className="btn btn-outline-light">
-                    На повышение
-            </button>
-            <button type="button"
-                    className="btn btn-outline-light">
-                    З/П больше 1000$
-            </button>
-        </div>
-    )
+class AppFilter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            allEmployees: true,
+            toRise: false,
+            overThousand: false
+
+        }
+    }
+
+
+    showAllEmployees = () => {
+        const newState = {
+            allEmployees: true,
+            toRise: false,
+            overThousand: false
+        }
+        this.setState(newState);
+        this.props.onUpdateFilter(newState)
+    }
+
+    showtoRise = () => {
+        const toRise = !this.state.toRise
+        const allEmployees = (this.state.overThousand || toRise) ? false : true
+        const newState = {
+            allEmployees: allEmployees,
+            toRise: toRise,
+            overThousand: this.state.overThousand,
+        }
+        this.setState(newState)
+        this.props.onUpdateFilter(newState)
+    }
+
+    showOverThousand = () => {
+        const overThousand = !this.state.overThousand
+        const allEmployees = (this.state.toRise || overThousand) ? false : true
+        const newState = {
+            allEmployees: allEmployees,
+            toRise: this.state.toRise,
+            overThousand: overThousand
+        }
+        this.setState(newState)
+        this.props.onUpdateFilter(newState)
+    }
+
+    render() {
+        const {allEmployees, toRise, overThousand} = this.state;
+        const allEmployeesClass = allEmployees ? "btn btn-light" : "btn btn-outline-light";
+        const toRiseClass = toRise ? "btn btn-light" : "btn btn-outline-light";
+        const overThousandClass = overThousand ? "btn btn-light" : "btn btn-outline-light";
+        return (
+            <div className="btn-group">
+                <button type="button"
+                        onClick={this.showAllEmployees}
+                        className={allEmployeesClass}>
+                        Все сотрудники
+                </button>
+                <button type="button"
+                onClick={this.showtoRise}
+                        className={toRiseClass}>
+                        На повышение
+                </button>
+                <button type="button"
+                        onClick={this.showOverThousand}
+                        className={overThousandClass}>
+                        З/П больше 1000$
+                </button>
+            </div>
+        )
+    }
 }
 
 export default AppFilter;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,11 @@ class App extends Component {
         { name: 'Kirill', salary: '2000', increase: false, rise: false, id: 1 },
         { name: 'Tanya', salary: '2000', increase: false, rise: true, id: 2 },
         { name: 'Busya', salary: '5000', increase: true, rise: false, id: 3 },
-      ]
+      ],
+      term: '',
+      allEmployees: true,
+      toRise: false,
+      overThousand: false
     }
     this.maxId = 4
   }
@@ -56,9 +60,41 @@ class App extends Component {
     }))
   }
 
+
+  searchAndFilteredEmp = (items, term, all, rise, thsnd) => {
+    if (!term.length && all) return items
+    let res = [];
+    if (term.length) res = items.filter(item => {
+      return item.name.indexOf(term) > -1
+    })
+    if (rise) res = items.filter(item => item.rise === rise)
+    if (thsnd) res = items.filter(item => item.salary > 1000)
+    if (rise && thsnd) res = items.filter(item => item.rise === rise && item.salary > 1000)
+    return res;
+  }
+
+  onUpdateSearch = (term) => {
+    this.setState({term}) // this.setState({term: term})
+  }
+
+  onUpdateFilter = (filterState) => {
+    this.setState(filterState)
+  }
+
+  // filteredEmp = (items, all, toRise, overThsnd) => { //
+  //   if (all) return items
+  //   let res = [];
+  //   if (toRise) res = items.filter(item => item.rise === toRise)
+    
+  //   return res
+  // }
+
   render() {
+    const {data, term, allEmployees, toRise, overThousand} = this.state
     const employees = this.state.data.length;
     const increased = this.state.data.filter(item => item.increase).length;
+    let visibleData = this.searchAndFilteredEmp(data, term, allEmployees, toRise, overThousand);
+    // visibleData = this.filteredEmp(data, allEmployees, toRise, overThousand)
 
     return (
       <div className="app">
@@ -67,12 +103,12 @@ class App extends Component {
           increased={increased}/>
   
         <div className="search-panel">
-          <SearchPanel />
-          <AppFilter />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+          <AppFilter onUpdateFilter={this.onUpdateFilter}/>
         </div>
   
         <EmployeesList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}/>
         <EmployeesAddForm
